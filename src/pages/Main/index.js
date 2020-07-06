b2a2eb528c2512732caa1389adea3f9f10997f7d
Fernando Meira <thomwork@outlook.com>
2020-07-06 00:43:49 -0300

    feat(form): start form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { RiTodoLine } from 'react-icons/ri';
 import { FaPlus } from 'react-icons/fa';
 
@@ -7,6 +7,18 @@ import { ImageComponent, InfoHeader } from '../../components';
 import { Container, Content, Form, SubmitButton, FormContent } from './styles';
 
 function Main() {
+  const [newTaks, setNewTask] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(newTaks);
+  }
+
   return (
     <>
       <Container>
@@ -17,15 +29,20 @@ function Main() {
 
           <FormContent>
             <h1>
-              <RiTodoLine />
-              ToDo List JWL
+              <RiTodoLine color="#5e68c3" />
+              Lista de tarefas
             </h1>
 
-            <Form onSubmit-={() => {}}>
-              <input type="text" placeholder="Adicionar uma tarefa" />
+            <Form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Adicionar uma tarefa"
+                value={newTaks}
+                onChange={handleInputChange}
+              />
 
               <SubmitButton>
-                <FaPlus size={20} color="#5E68C3" />
+                <FaPlus size={20} color="#fff" />
               </SubmitButton>
             </Form>
           </FormContent>
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { pxToRem } from '../../functions';
+
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
@@ -18,8 +20,53 @@ export const Content = styled.div`
   flex-direction: row;
 `;
 
-export const FormContent = styled.div``;
+export const FormContent = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  flex: 1;
 
-export const Form = styled.form``;
+  > h1 {
+    display: flex;
+    align-items: center;
 
-export const SubmitButton = styled.button``;
+    font-weight: 500;
+
+    & svg {
+      margin-right: ${pxToRem(8)};
+    }
+  }
+`;
+
+export const Form = styled.form`
+  min-width: 500px;
+
+  margin-top: ${pxToRem(32)};
+  display: flex;
+  flex-direction: row;
+
+  & input {
+    padding: 8px 12px;
+    border-radius: 4px;
+    border: 1px solid #eee;
+
+    flex: 1;
+
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  border: 0;
+  padding: 0 10px;
+  margin-left: ${pxToRem(8)};
+  border-radius: ${pxToRem(4)};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: #5e68c3;
+`;
